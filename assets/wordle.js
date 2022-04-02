22fe5fdfece26ae00463482e447d63f7cd328280
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-02 14:36:34 -0700

    fixed html file

---
+++ assets/wordle.js
@@ -0,0 +1,22 @@
+var words = ["ORANGES", "PIZZA", "PEARS", "CHIPS"];
+var wordIndex = 0;
+var wins = 0;
+var losses = 0;
+var button = document.querySelector(".button");
+
+// Start game by clicking button 
+function 
+    //timer should reset and start (30s)
+//Fill in the blanks to match the number of letters in word 
+    //Key event: when a letter key is pushed
+    //Correct guess: letter should replace the blank
+    //5 words 
+//Timer 
+//Win game when all letters of word have been guessed
+    //Message: you win!
+//Lose the game 
+    //When timer runs out
+    //Message: you lost!
+//Total wins and losses 
+//Refresh page
+    //Wins and losses stay the same 
